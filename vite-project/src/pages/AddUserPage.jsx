93c258bed7f675436ada778bd71f5b1660042596
sagarrathod7568 <sagarrathod050@gmail.com>
2024-11-18 12:33:24 +0530

    updated

--- vite-project/src/pages/AddUserPage.jsx
+++
@@ -1,6 +0,0 @@
-// import React from "react";
-import UserForm from "../components/UserForm";
-
-const AddUserPage = () => <UserForm isEdit={false} />;
-
-export default AddUserPage;
--- vite-project/src/pages/HomePage.jsx
+++
@@ -1,6 +0,0 @@
-// import React from "react";
-import UserList from "../components/UserList";
-
-const HomePage = () => <UserList />;
-
-export default HomePage;
